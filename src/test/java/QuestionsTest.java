d4d934adcf7840c4f041ebf5eaf9e9049cd1fbad
Samuel Kanu <samuelkanu35@gmail.com>
2023-03-04 18:52:45 +0000

    Completd

--- src/test/java/QuestionsTest.java
+++ src/test/java/QuestionsTest.java
@@ -4,16 +4,20 @@ import org.junit.Test;
 public class QuestionsTest {
     Questions q = new Questions();
 
+    /**
+     * 
+     */
     @Test
     public void testQuestion1() {
-        int expectedResult = (int) Math.PI / 1;
+        int expectedResult = (int) Math.PI / 1
+        ;
         int actualResult = q.question1();
         Assert.assertEquals(expectedResult, actualResult);
     }
 
     @Test
     public void testQuestion2() {
-        int expectedResult = (int) Math.pow(2, 4) - 2;
+        int expectedResult = (int) Math.pow(2, 4) - 0;
         int actualResult = q.question2();
         Assert.assertEquals(expectedResult, actualResult);
     }
